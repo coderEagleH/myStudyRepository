f7fc779831fa39bb07b615bc11da60dc0eb5c40b
BlackCrow <1286384262@qq.com>
2021-01-20 14:55:06 +0800

    .

---
+++ studyCase_weatherQuery/js/main.js
@@ -0,0 +1,38 @@
+/* 
+
+    请求地址：http://wthrcdn.etouch.cn/weather_mini
+    请求方式：get
+    请求参数：city(城市名)
+    相应内容：天气信息
+
+*/
+var app = new Vue({
+    el: "#skyKnow",
+    data: {
+        cities: ['北京', '上海', '广州', '深圳', '重庆'],
+        city: "",
+        topCity: "",
+        weatherList: []
+    },
+    methods: {
+        searching: function () {
+            var that = this;
+            axios.get("http://wthrcdn.etouch.cn/weather_mini?city=" + this.city).then(
+                function (responce) {
+                    that.weatherList = responce.data.data.forecast;
+                    that.topCity = that.city;
+                },
+                function (err) {
+                    that.topCity = "仅支持按城市搜索，请重新输入"
+
+                })
+        },
+        clickSearch: function (index) {
+            this.city = this.cities[index];
+            this.searching();
+        },
+        onFocus: function () {
+            this.city = "";
+        }
+    }
+})
